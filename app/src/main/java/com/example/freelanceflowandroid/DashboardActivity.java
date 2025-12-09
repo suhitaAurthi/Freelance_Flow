e2da85c4ee6eb7b144d20555d7d1f90254514861
suhitaAurthi <aurthi2020@gmail.com>
2025-12-09 10:43:28 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/freelanceflowandroid/DashboardActivity.java
@@ -0,0 +1,12 @@
+package com.example.freelanceflowandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
